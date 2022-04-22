501de73b8d70eb77ce1ec7177f1325902b7bd9c3
Dardrijeri <dardrijeri@gmail.com>
2022-04-22 20:22:08 +0300

    Fixed get_length

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ def get_length(input_video):
     result = subprocess.run(['ffprobe', '-v', 'error', '-show_entries',
                              'format=duration', '-of', 'default=noprint_wrappers=1:nokey=1', input_video],
                             stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    return result.stdout
+    return float(result.stdout.splitlines()[0])
 
 
 if __name__ == "__main__":
